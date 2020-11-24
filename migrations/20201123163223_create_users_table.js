6b13adb22565d13cb5522629546b30b5ba8b89a3
Michaela Bateman <michaela.bateman001@gmail.com>
2020-11-24 15:05:57 -0600

    changes to db and most of the scrape file is finished

--- migrations/20201123163223_create_users_table.js
+++ migrations/20201123163223_create_users_table.js
@@ -3,22 +3,25 @@ exports.up = function(knex) {
     return knex.schema.createTable('users', tbl => {
         tbl.increments() //auto id
         tbl.text('username').notNullable()
-        tbl.text('bio')
-        tbl.text('location')
+        tbl.text('avatar_url')
         tbl.text('user_url')
+        tbl.integer('github_id').notNullable()
 
     })
     .createTable('repos', tbl => {
         tbl.increments()
         tbl.integer('user_id') //Foreign key associated with users.id
             .unsigned()
-            .references('id')
+            .references('github_id')
             .inTable('users')
             .onDelete('CASCADE')
             .onUpdate('CASCADE')
         tbl.text('name').notNullable()
+        tbl.text('fullname')
         tbl.boolean('public_access')
         tbl.text('repo_url')
+        tbl.text('description')
+        tbl.text('language')
     })
 };
 
--- src/scrapeGitHub.js
+++ src/scrapeGitHub.js
@@ -2,30 +2,69 @@ const config = require('../knexfile')
 const knex = require('knex')
 const db = knex(config.development)
 const RepoService = require('../src/repos/repos-service')
+const fetch = require('node-fetch')
+const UserService = require('./users/users-service')
 
 function scrape(){
     console.log('scraping')
-
-    fetch('https://api.github.com/repositories')
-    .then(res => {
-        console.log(res.length)
-        const newRepos
-        //pull data we want from the res
-        res.map((x, i) => {
-
-        })
-        
-    })
+    
+    return fetch('https://api.github.com/users') //fetching large amounts of users
+            .then(res => res.json())
+            .then(body => {
+                console.log(body)
+                if(body.message){
+                    console.log(body.message)
+                }else{
+                    let newEntries = []
+                    body.forEach((user) => { //for each user we are conforming res data for our database
+                        let newUser = {
+                            "username": user.login,
+                            "avatar_url": user.avatar_url,
+                            "user_url": user.html_url,
+                            "github_id": user.id
+                        }
+                        // compileRepos(user.repos_url) //gathering all repos associated with each user
+                        newEntries.push(newUser)
+                    })
+            }
+                // UserService.insertUsers(newEntries)
+                // .then((res) => {
+                //     console.log(res)
+                //     console.log('users successfully scraped')
+                // })
+            })
+            .catch()
 }
 
+function compileRepos(url){
+    
+  return fetch(url)
+        .then(res => res.json())
+        .then(body => {
+        let usersRepos = []
+
+        body.forEach(repo => {
+            let newRepo = {
+                "name": repo.name,
+                "description": repo.description,
+                "fullname": repo.full_name,
+                "private": repo.private,
+                "user_id": repo.owner.id,
+                "repo_url": repo.html_url,
+                "fork": repo.fork,
+                "language": repo.language
+            }
+
+            usersRepos.push(newRepo)
+        })
+
+        RepoService.insertRepos(usersRepos)
+        .then(res => {
+            console.log(res)
+            console.log('repos successfully scraped')
+        })
+    })
+}
 scrape()
-// .then(() => {
-//     console.log('scraping successful')
-//     process.exit()
-// })
-// .catch((e) => {
-//     console.log('scraping unsuccessful')
-//     process.exit()
-// })
 
 module.exports = scrape
