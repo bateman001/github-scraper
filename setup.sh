898111a85269082827ea0cc68b42029f5f229ef8
Michaela Bateman <michaela.bateman001@gmail.com>
2020-11-29 16:14:35 -0600

    added node env to setup

--- setup.sh
+++ setup.sh
@@ -5,7 +5,8 @@ npm install
 npm run migrate:down
 npm run migrate
 touch .env
-echo 'AUTH_TOKEN="2ed2a997fc7cab47b5862b7b2ee840d93bb130e0"' >> .env
+echo 'AUTH_TOKEN=""' >> .env
+echo 'NODE_ENV=developement' >> .env
 echo finsihed
 
 
