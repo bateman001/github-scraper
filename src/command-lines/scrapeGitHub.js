83405a6cef9a7bf2e0959efc9be232f37a88f459
Michaela Bateman <michaela.bateman001@gmail.com>
2020-11-29 20:47:32 -0600

    added dynamic scraping

--- src/command-lines/scrapeGitHub.js
+++ src/command-lines/scrapeGitHub.js
@@ -1,11 +1,19 @@
 const getUserFromApi = require('./api-service/getUserFromApi')
 const getReposFromApi = require('./api-service/getReposFromApi')
 const colors = require('colors')
+const UserService = require('../users/users-service')
 
 async function scrape(){
+    let urls
+    const users = await UserService.getAllUsers()
+
     console.log('\nscraping users'.blue)
-    const urls = await getUserFromApi('https://api.github.com/users') 
-    console.log('\nscraping users repos'.blue)
+    if(users.length === 0){
+        urls = await getUserFromApi(`https://api.github.com/users`) 
+    }else{
+        urls = await getUserFromApi(`https://api.github.com/users?since=${users[users.length - 1].github_id}`)
+    }
+    console.log('\nscraping users repos... this may take a second'.blue)
     return getReposFromApi(urls)
 }
 
